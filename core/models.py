b0fae19fe8bebacb61c39c57f06a2a1a372c5ffb
Igor Baiocco <igor64br@gmail.com>
2021-09-05 16:27:58 -0300

    'v0.1 - Allowed event creation'

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Evento(models.Model):
+    nome = models.CharField(max_length=100)
+    organizadores = models.TextField()
+    inicio = models.IntegerField()
+    fim = models.IntegerField()
+    confirmado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
