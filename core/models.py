553ac17069c165f987ddc157c8a875a8f807da66
Igor Baiocco <igor64br@gmail.com>
2021-09-05 17:32:06 -0300

    'v0.1 - Blank README.md created'

--- core/models.py
+++ core/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+# from organizadores.models import Organizadore
 
 
 # Create your models here.
 class Evento(models.Model):
     nome = models.CharField(max_length=100)
-    organizadores = models.TextField()
-    inicio = models.IntegerField()
-    fim = models.IntegerField()
     confirmado = models.BooleanField(default=False)
+    # data_de_inicio = models.DateTimeField(auto_now_add=False)
+    # data_de_fim = models.DateTimeField(auto_now_add=False)
+    # orgamizadores = models.ManyToManyField(Organizadore)
 
     def __str__(self):
         return self.nome
