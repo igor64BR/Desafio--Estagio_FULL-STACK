8b0ca7990c9a801e0d851c6c16482cb9dcfd27d7
Igor Baiocco <igor64br@gmail.com>
2021-09-06 08:51:20 -0300

    'v0.2 - Forms dos eventos e organizadores prontos'

--- core/models.py
+++ core/models.py
@@ -1,14 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
-# from organizadores.models import Organizadore
+from django.forms import DateInput
+from organizadores.models import Organizadore
 
 
 # Create your models here.
 class Evento(models.Model):
     nome = models.CharField(max_length=100)
+    organizadores = models.ManyToManyField(Organizadore)
+    # organizadores = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
     confirmado = models.BooleanField(default=False)
-    # data_de_inicio = models.DateTimeField(auto_now_add=False)
-    # data_de_fim = models.DateTimeField(auto_now_add=False)
-    # orgamizadores = models.ManyToManyField(Organizadore)
 
     def __str__(self):
         return self.nome
--- eventos/settings.py
+++ eventos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'organizadores'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
